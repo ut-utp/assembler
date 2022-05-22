e06502829cd3091b70d59c97b98c33322c782fc9
David Gipson <david.gipson@utexas.edu>
2022-05-22 13:37:17 -0500

    assembler: update to clap v3, improve CLI help info

--- assembler/bin/as.rs
+++ assembler/bin/as.rs
@@ -6,7 +6,7 @@ use std::process::exit;
 use ariadne::Source;
 use lc3_assembler::parser::parse;
 use lc3_shims::memory::FileBackedMemoryShim;
-use clap::clap_app;
+use clap::{Parser};
 use lc3_assembler::analysis::{report, validate};
 use lc3_assembler::assembler::assemble;
 use lc3_assembler::LeniencyLevel;
@@ -14,6 +14,37 @@ use lc3_assembler::lexer::lex;
 use lc3_assembler::linker::link;
 
 const MEM_DUMP_FILE_EXTENSION: &'static str = "mem";
+
+#[derive(Parser)]
+#[clap(author, version, about,
+    long_about = "Analyzes, assembles, and/or links LC-3 assembly and object files. \
+                  Each given assembly file is assembled to a single object file, \
+                  then all assembled or given object files are linked into a single executable image \
+                  of LC-3 machine code."
+      )]
+struct Args {
+    /// Input file paths
+    #[clap(required = true, parse(from_os_str), value_name = "INPUT_FILE")]
+    input: Vec<PathBuf>,
+
+    /// Enforce all rules of the original LC-3 assembly language
+    ///
+    /// By default, the assembler is lenient about restrictions such as label length.
+    /// This option enforces restrictions specified in Patt and Patel's Introduction to Computing Systems, 3rd edition.
+    // TODO: provide full list of restrictions
+    #[clap(long, short)]
+    strict: bool,
+
+    /// Check the correctness of the program without assembling
+    #[clap(long, short)]
+    check: bool,
+
+    /// Link executable image without OS
+    ///
+    /// If not specified, the program is overlaid onto an image of the OS from lc3-os at link time.
+    #[clap(long, short)]
+    no_os: bool,
+}
 
 fn main() {
     std::thread::Builder::new()
@@ -24,23 +55,14 @@ fn main() {
 }
 
 fn as_() {
-    let matches = clap_app!(assemble_lc3 =>
-        (version: env!("CARGO_PKG_VERSION"))
-        (author: env!("CARGO_PKG_AUTHORS"))
-        (about: env!("CARGO_PKG_DESCRIPTION"))
-        (@arg  strict: -s --strict   "Enforces all rules of the original LC-3 assembly language when validating the program")
-        (@arg   check: -c --check    "Checks the correctness of the program without attempting to assemble it")
-        (@arg with_os: -o --with_os  "Overlays the program onto an image of the OS from lc3-os")
-        (@arg   INPUT: +required ... "Paths to the programs to assemble")
-    ).get_matches();
+    let args = Args::parse();
 
-    for path_str in matches.values_of("INPUT").unwrap() {
-        let path = Path::new(path_str);
+    for path in args.input {
         assert!(path.is_file());
 
-        let leniency = if matches.is_present("strict") { LeniencyLevel::Strict } else { LeniencyLevel::Lenient };
+        let leniency = if args.strict { LeniencyLevel::Strict } else { LeniencyLevel::Lenient };
 
-        let string = fs::read_to_string(path).unwrap();
+        let string = fs::read_to_string(path.clone()).unwrap();
         let src = string.as_str();
 
         let (maybe_tokens, lex_errs) = lex(src, leniency);
@@ -49,7 +71,6 @@ fn as_() {
         let (maybe_file, parse_errs) = parse(src, tokens, leniency);
         let (mut file, span) = maybe_file.expect("parsing failed");
 
-        println!("{:?}", file);
         let errors = validate(&file);
 
         if !errors.is_empty() {
@@ -60,10 +81,10 @@ fn as_() {
             continue;
         }
 
-        if matches.is_present("check") {
-            println!("{}: No errors found.", path_str);
+        if args.check {
+            println!("{}: No errors found.", path.to_str().unwrap());
         } else {
-            let background = if matches.is_present("with_os") { Some(lc3_os::OS_IMAGE.clone()) } else { None };
+            let background = if args.no_os { None } else { Some(lc3_os::OS_IMAGE.clone()) };
 
             let objects =
                 file.into_iter()
@@ -71,7 +92,7 @@ fn as_() {
 
             let mem = link(objects, background);
 
-            let mut output_path = PathBuf::from(path_str);
+            let mut output_path = path.clone();
             output_path.set_extension(MEM_DUMP_FILE_EXTENSION);
             let mut file_backed_mem = FileBackedMemoryShim::with_initialized_memory(output_path, mem);
             file_backed_mem.flush_all_changes().unwrap();
